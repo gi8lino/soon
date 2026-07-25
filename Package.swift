76d610f40b2cb42446a24667bc511a2aac6fcdac
gi8 <giottolino@gmail.com>
2026-07-25 22:28:23 +0200

    bump packages

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
   ],
   dependencies: [
     //.package(path: "../easybar"),
-    .package(url: "https://github.com/gi8lino/easybar", from: "0.30.3"),
+    .package(url: "https://github.com/gi8lino/easybar", from: "0.30.4"),
     .package(
       url: "https://github.com/gi8lino/SwiftTOMLEdit.git",
-      exact: "0.0.3"
+      exact: "0.0.2"
     ),
   ],
   targets: [
